a229f3a0e05886c2d52b96f721058a074236cb06
Matthew Elphick <matt.a.elphy@gmail.com>
2023-02-09 18:59:37 +0000

    chore: More to help debug webhooks

--- web/src/pages/api/test/webhook.tsx
+++ web/src/pages/api/test/webhook.tsx
@@ -28,6 +28,27 @@ const handler: NextApiHandler = async (req, res) => {
     })
     const data = await fetchRes.json()
     res.json({ type, data })
+  } else if (type === 'patch') {
+    const fetchRes = await fetch('https://www.patreon.com/api/oauth2/v2/webhooks/526532', {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        data: {
+          id: '526532',
+          type: 'webhook',
+          attributes: {
+            triggers: ['members:delete', 'posts:publish', 'posts:update'],
+            uri: 'https://patreon-herald.mael.tech/api/webhooks/patreon/63e52b8e8a76b230774bab57',
+            paused: 'false', // <- do this if you’re attempting to send missed events, see NOTE in Example Webhook Payload
+          },
+        },
+      }),
+    })
+    const data = await fetchRes.json()
+    res.json({ type: 'get', data })
   } else {
     const fetchRes = await fetch('https://www.patreon.com/api/oauth2/v2/webhooks', {
       method: 'GET',
@@ -39,7 +60,6 @@ const handler: NextApiHandler = async (req, res) => {
     const data = await fetchRes.json()
     res.json({ type: 'get', data })
   }
-  res.json({ missed: type })
 }
 
 export default handler
