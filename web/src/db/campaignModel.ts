9dc4827f55c12dfa3430325c3b78723e3c55c402
Matthew Elphick <matt.a.elphy@gmail.com>
2023-03-18 22:49:37 +0000

    feat: Add basics for entitlements to campaign model

--- web/src/db/campaignModel.ts
+++ web/src/db/campaignModel.ts
@@ -5,9 +5,17 @@ type WithDoc<T> = T
 export interface Campaign {
   patreonCampaignId: string
   ownerPatreonId: string
+  entitledCriteria: {
+    criteriaType: string
+    amountCents: number
+    tierId: string
+  }
   sounds: {
     [k: string]: {
       sound: string
+      currentlyEntitledAmountsCents: number
+      currentlyEntitledTiers: string[]
+      lifetimeSupportCents: number
       isApproved: boolean
       isRejected: boolean
     }
@@ -24,11 +32,24 @@ const itemSchema = new Schema<WithDoc<Campaign>, ItemModel>(
   {
     patreonCampaignId: { required: true, type: String },
     ownerPatreonId: { required: true, type: String },
+    entitledCriteria: {
+      type: {
+        criteriaType: {
+          type: String,
+          default: 'currently_entitled',
+        },
+        amountCents: Number,
+        tierId: String,
+      },
+    },
     sounds: {
       default: {},
       type: Map,
       of: {
         sound: String,
+        currentlyEntitledAmountsCents: { default: 0, type: Number },
+        currentlyEntitledTiers: { default: [], type: Array, of: String },
+        lifetimeSupportCents: { default: 0, type: Number },
         isApproved: { type: Boolean, default: false },
         isRejected: { type: Boolean, default: false },
       },
