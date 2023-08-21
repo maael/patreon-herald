797f9e513fb80857f6b0dfd09557d4631ff5a295
Matthew Elphick <matt.a.elphy@gmail.com>
2023-08-21 08:01:42 +0100

    feat: Start changing volume in overlay

--- web/src/pages/obs/alert/[twitch]/[id].tsx
+++ web/src/pages/obs/alert/[twitch]/[id].tsx
@@ -1,7 +1,22 @@
 import { useRouter } from 'next/router'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useQuery } from 'react-query'
 import tmi from 'tmi.js'
+
+function makeAudio(target: HTMLAudioElement, audio?: any) {
+  if (audio) {
+    if (audio.ctx.state === 'suspended') {
+      audio.ctx.resume()
+    }
+    return audio
+  }
+  const ctx = new (window.AudioContext || (window as any).webkitAudioContext)()
+  const source = ctx.createMediaElementSource(target)
+  const gain = ctx.createGain()
+  source.connect(gain)
+  gain.connect(ctx.destination)
+  return { ctx, source, gain }
+}
 
 export default function Alert() {
   const seenList = useRef<Set<string>>(new Set())
@@ -28,18 +43,33 @@ export default function Alert() {
           )
         }),
   })
+  const [audio, setAudio] = useState<
+    | {
+        ctx: AudioContext
+        source: MediaElementAudioSourceNode
+        gain: GainNode
+      }
+    | undefined
+  >()
   console.info('[loaded]', { count: data?.size })
   useEffect(() => {
     console.info('[connect]', { twitch })
     const client = new tmi.Client({ channels: [twitch] })
+
+    if (ref.current) setAudio(makeAudio(ref.current, audio))
 
     client.connect()
 
     client.on('message', (_channel, tags) => {
       const userId = tags['user-id']
       const sound = data?.get(userId)?.sound
-      if (ref.current && sound && !seenList.current.has(userId)) {
-        console.info('[playing]', { userId, displayName: tags['display-name'], sound })
+      if (ref.current && sound) {
+        // && !seenList.current.has(userId)) {
+        console.info('[playing]', { userId, displayName: tags['display-name'], sound, audio })
+        if (audio) {
+          console.info('[audio:volume]', 5)
+          audio.gain.gain.value = 0.1
+        }
         seenList.current.add(userId)
         ref.current.src = sound
         ref.current.pause()
@@ -52,10 +82,21 @@ export default function Alert() {
     return () => {
       client.disconnect()
     }
-  }, [data, twitch])
+  }, [data, twitch, audio, setAudio])
 
   return (
-    <audio controls ref={ref} className="invisible">
+    <audio
+      controls
+      ref={ref}
+      className="invisible"
+      crossOrigin="anonymous"
+      onPlay={(e) => {
+        setAudio(makeAudio(e.currentTarget, audio))
+      }}
+      onLoadStart={(e) => {
+        setAudio(makeAudio(e.currentTarget, audio))
+      }}
+    >
       <source />
     </audio>
   )
